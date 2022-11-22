cdbe0dec92c2041b00e795d7504889fa9af6223c
DimaLite <lucenkodima816@gmail.com>
2022-11-22 13:08:21 +0300

    added slider code

--- js/main.js
+++ js/main.js
@@ -1,10 +1,45 @@
-let name = prompt("What is your name?");
+// let name = prompt("What is your name?");
 
-alert("Hi, " + name + "! Welcome!");
+// alert("Hi, " + name + "! Welcome!");
 
 
-let btns = document.querySelectorAll('.slider__arrow');
-btns[0].addEventListener('click', ha)
-function ha(){
-    alert('Ha-ha')
+const btns = document.querySelectorAll('.slider__arrow'),
+    elementsSlider = document.querySelectorAll('.slider__elem'),
+    next = btns[1],
+    prev = btns[0];
+let index = 0;
+
+const activeSlide = n => {
+    for (let slide of elementsSlider) {
+        slide.classList.remove('slider__elem--active');
+    }
+    elementsSlider[n].classList.add('slider__elem--active');
 }
+
+const prepareCurrentSlide = () =>{
+    activeSlide(index);
+
+}
+
+const nextSlide = () => {
+    if (index == elementsSlider.length - 1) {
+        index = 0;
+        prepareCurrentSlide(index);
+    } else {
+        index++;
+        prepareCurrentSlide(index);
+    }
+}
+
+const prevSlide = () => {
+    if (index == 0) {
+        index = elementsSlider.length - 1;
+        prepareCurrentSlide(index);
+    } else {
+        index--;
+        prepareCurrentSlide(index);
+    }
+}
+
+next.addEventListener('click', nextSlide);
+prev.addEventListener('click', prevSlide);
