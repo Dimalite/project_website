e71eaaaf89995fc1bd5a31e6ffa08fc7cf180bc9
DimaLite <lucenkodima816@gmail.com>
2022-11-22 10:28:30 +0300

    Added main.js file

--- js/main.js
+++ js/main.js
@@ -1,5 +1,10 @@
-// let name = prompt("What is your name?");
+let name = prompt("What is your name?");
 
-// alert("Hi, " + name + "! Welcome!");
+alert("Hi, " + name + "! Welcome!");
 
 
+let btns = document.querySelectorAll('.slider__arrow');
+btns[0].addEventListener('click', ha)
+function ha(){
+    alert('Ha-ha')
+}
