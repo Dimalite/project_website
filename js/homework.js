020e2abb90d399cd4be204b435e0f12e89dcef2e
DimaLite <lucenkodima816@gmail.com>
2022-11-26 00:36:02 +0300

    Added homework.js

--- js/homework.js
+++ js/homework.js
@@ -1,2 +1,25 @@
-// let name = prompt("What is your name?");
-// alert("Hi, " + name + "! Welcome!");
+const ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+function randomCharacterForString() {
+  return ALPHABET.charAt(Math.floor(Math.random() * ALPHABET.length));
+}
+
+console.log(randomCharacterForString());
+
+/// ======== Puzzle with an asterisk! ===========================
+
+let price = "$120";
+let dollarRate = "41.53 ₴";
+let totalPrice = "Cost is $158.73";
+
+function extractCurrencyValue(str) {
+  if (str.includes(".")) {
+    console.log(str.replace(/[^0-9.]/g, ""));
+  } else {
+    console.log(str.replace(/\D/g, ""));
+  }
+}
+
+extractCurrencyValue(price);
+extractCurrencyValue(dollarRate);
+extractCurrencyValue(totalPrice);
