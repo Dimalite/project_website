ecbbfdab779cde83b293204d03b8eabbfc51d0dc
DimaLite <lucenkodima816@gmail.com>
2022-12-04 23:23:08 +0300

    added counters

--- js/homework.js
+++ js/homework.js
@@ -1,25 +1,59 @@
-const ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+// const ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-function randomCharacterForString() {
-  return ALPHABET.charAt(Math.floor(Math.random() * ALPHABET.length));
+// function randomCharacterForString() {
+//   return ALPHABET.charAt(Math.floor(Math.random() * ALPHABET.length));
+// }
+
+// console.log(randomCharacterForString());
+
+// /// ======== Puzzle with an asterisk! ===========================
+
+// let price = "$120";
+// let dollarRate = "41.53 ₴";
+// let totalPrice = "Cost is $158.73";
+
+// function extractCurrencyValue(str) {
+//   if (str.includes(".")) {
+//     console.log(str.replace(/[^0-9.]/g, ""));
+//   } else {
+//     console.log(str.replace(/\D/g, ""));
+//   }
+// }
+
+// extractCurrencyValue(price);
+// extractCurrencyValue(dollarRate);
+// extractCurrencyValue(totalPrice);
+
+/*           HOMEWORK ARRAYs      */
+//  ^^^^^^^^^^^^^^^^^ Task #1  ^^^^^^^^^^^^^^^^^^^^^^^^^^^
+let d1 = [45, 78, 10, 3];
+d1[7] = 100; // Add one more number to array d1 ( d1[7] = 100)
+console.log(d1, d1[6], d1[7]); // Out the array and its elements with indices 6 and 7.
+
+// ^^^^^^^^^^^^^^^^^ Task #2 ^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+let d2 = [45, 78, 10, 3];
+let sum2 = 0;
+// method for - Sum of numbers in an array
+for (i = 0; i < d2.length; i++) {
+  sum2 += d2[i];
+}
+// 136
+
+//^^^^^^^^^^^^^^^^^^ Task #3 ^^^^^^^^^^^^^^^^^^^^^^^^^^^
+let d3 = [45, 78, 10, 3];
+d3[7] = 100;
+let sum3 = 0;
+// method for in - Sum of numbers in an array
+for (let num in d3) {
+  sum3 += d3[num];
 }
 
-console.log(randomCharacterForString());
+//  ^^^^^^^^^^^^^^^^^^ Task #4 ^^^^^^^^^^^^^^^^^^^^^^^^^^^
+let d4 = [45, 78, 10, 3];
 
-/// ======== Puzzle with an asterisk! ===========================
-
-let price = "$120";
-let dollarRate = "41.53 ₴";
-let totalPrice = "Cost is $158.73";
-
-function extractCurrencyValue(str) {
-  if (str.includes(".")) {
-    console.log(str.replace(/[^0-9.]/g, ""));
-  } else {
-    console.log(str.replace(/\D/g, ""));
-  }
+// The function sorts the elements of an array in descending order.
+function my(a, b) {
+  return b - a;
 }
 
-extractCurrencyValue(price);
-extractCurrencyValue(dollarRate);
-extractCurrencyValue(totalPrice);
+d4.sort(my); // [78, 45, 10, 3]
--- js/main.js
+++ js/main.js
@@ -39,3 +39,33 @@ const prevSlide = () => {
 next.addEventListener('click', nextSlide);
 prev.addEventListener('click', prevSlide);
 
+// counters
+window.onscroll = function () {
+    
+    var count_1 = document.getElementById('num1')
+    var count_2 = document.getElementById('num2')
+    var count_3 = document.getElementById('num3')
+
+    var count1 = 0;
+    var project1 = setInterval(counter1, 10);
+    var count2 = 0;
+    var project2 = setInterval(counter2, 1);
+    var count3 = 0;
+    var project3 = setInterval(counter3, 10);
+    
+    function counter1() {
+        count1++;    
+        count_1.innerHTML = count1;
+        if (count1 == 120) clearInterval(project1);
+    }
+    function counter2() {
+        count2++;    
+        count_2.innerHTML = count2;
+        if (count2 == 200) clearInterval(project2);
+    }
+    function counter3() {
+        count3++;    
+        count_3.innerHTML = count3;
+        if (count3 == 75) clearInterval(project3);
+    }
+}
